585010de82feeaecb976ad51ce7a7f1d855da867
andrewshan <andrewshan@tencent.com>
2022-01-10 16:35:10 +0800

    feat: 支持eureka的样例创建

---
+++ eureka/eureka-java/consumer/src/main/java/cn/polarismesh/examples/eureka/consumer/Config.java
@@ -0,0 +1,16 @@
+package cn.polarismesh.examples.eureka.consumer;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
