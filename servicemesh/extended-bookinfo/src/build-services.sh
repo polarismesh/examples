e735f7a05a7a38f6376020b880af274992fb5169
andrewshan <andrewshan@tencent.com>
2022-07-14 15:00:07 +0800

    fix: review docker build

--- servicemesh/extended-bookinfo/src/build-services.sh
+++ servicemesh/extended-bookinfo/src/build-services.sh
@@ -26,6 +26,19 @@ VERSION=$1
 PREFIX=$2
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+pushd "$SCRIPTDIR/reviews"
+  #java build the app.
+  docker run --rm -u root -v "$(pwd)":/home/maven/project -w /home/maven/project maven:3.8.1-openjdk-8-slim mvn clean package
+  #plain build -- no ratings
+  docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v1:latest" --build-arg service_version=v1 .
+  #with ratings black stars
+  docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v2:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v2:latest" --build-arg service_version=v2 \
+   --build-arg enable_ratings=true .
+  #with ratings red stars
+  docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v3:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v3:latest" --build-arg service_version=v3 \
+   --build-arg enable_ratings=true --build-arg star_color=red .
+popd
+
 pushd "$SCRIPTDIR/productpage"
   docker build --pull -t "${PREFIX}/examples-bookinfo-productpage-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-productpage-v1:latest" .
   #flooding
@@ -38,21 +51,6 @@ pushd "$SCRIPTDIR/details"
   #with calling external book service to fetch topic for the book
   docker build --pull -t "${PREFIX}/examples-bookinfo-details-v2:${VERSION}" -t "${PREFIX}/examples-bookinfo-details-v2:latest" --build-arg service_version=v2 \
 	 --build-arg enable_external_book_service=true .
-popd
-
-pushd "$SCRIPTDIR/reviews"
-  #java build the app.
-  docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
-  pushd reviews-wlpcfg
-    #plain build -- no ratings
-    docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v1:latest" --build-arg service_version=v1 .
-    #with ratings black stars
-    docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v2:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v2:latest" --build-arg service_version=v2 \
-	   --build-arg enable_ratings=true .
-    #with ratings red stars
-    docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v3:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v3:latest" --build-arg service_version=v3 \
-	   --build-arg enable_ratings=true --build-arg star_color=red .
-  popd
 popd
 
 pushd "$SCRIPTDIR/ratings"
