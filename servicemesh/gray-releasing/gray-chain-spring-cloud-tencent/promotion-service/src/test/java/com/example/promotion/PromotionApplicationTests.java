5a35a77c0e398a599a2a62c61e30eac865d3ed00
chuntaojun <liaochuntao@live.com>
2022-09-26 11:47:54 +0800

    feat:新增gray的sctdemo

---
+++ servicemesh/gray-releasing/gray-chain-spring-cloud-tencent/promotion-service/src/test/java/com/example/promotion/PromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
